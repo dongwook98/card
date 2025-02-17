69c764d99c78945024e5caf1966f9e9a273a974a
강동욱 <ehddnr8563@gmail.com>
2025-02-18 03:11:35 +0900

    feat: Alert를 Context로 관리

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import Text from '@shared/Text';
 import Button from '@shared/Button';
 import Input from './components/shared/Input';
 import TextField from './components/shared/TextField';
+import { useAlertContext } from './contexts/AlertContext';
 
 function App() {
+  const { open } = useAlertContext();
   return (
     <>
       <Text typography='t1' color='blue'>
@@ -14,7 +16,6 @@ function App() {
       <Text typography='t4'>t4</Text>
       <Text typography='t5'>t5</Text>
 
-      <div></div>
       <Button color='succuss'>클릭해주세요</Button>
       <Button color='error'>클릭해주세요</Button>
       <Button color='primary'>클릭해주세요</Button>
@@ -36,6 +37,20 @@ function App() {
       <Input aria-invalid={true} />
       <TextField label='아이디' />
       <TextField label='패스워드' hasError />
+
+      <Button
+        onClick={() => {
+          open({
+            title: '카드신청완료',
+            description: '내역페이지에서 확인',
+            onButtonClick: () => {
+              console.log('클릭');
+            },
+          });
+        }}
+      >
+        Alert
+      </Button>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import { Global } from '@emotion/react';
 import globalStyles from './styles/globalStyles.ts';
 
 import App from './App.tsx';
+import { AlertContextProvider } from '@contexts/AlertContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* Emotion 글로벌 스타일 적용 */}
     <Global styles={globalStyles} />
-    <App />
+    <AlertContextProvider>
+      <App />
+    </AlertContextProvider>
   </StrictMode>
 );
