33ea237e3ea7145644f7cfad7ee86c892aafa9da
강동욱 <ehddnr8563@gmail.com>
2025-02-17 18:27:47 +0900

    chore: 컬러팔레트, 타이포그래피, 글로벌 스타일 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { Global } from '@emotion/react';
+import globalStyles from './styles/globalStyles.ts';
+
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* Emotion 글로벌 스타일 적용 */}
+    <Global styles={globalStyles} />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
