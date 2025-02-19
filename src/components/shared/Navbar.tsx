8a9a8be173f600908732d397ac5a39082eae8640
강동욱 <ehddnr8563@gmail.com>
2025-02-20 03:47:51 +0900

    feat: 로그아웃 구현 및 user 상태에 따른 버튼 렌더링 구현

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,23 +1,45 @@
+import { useCallback } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { css } from '@emotion/react';
+import { signOut } from 'firebase/auth';
 
 import Flex from './Flex';
 import Button from './Button';
-import { css } from '@emotion/react';
 import { colors } from '@/styles/colorPalette';
+import useUser from '@hooks/useUser';
+import { auth } from '@remote/firebase';
 
 export default function Navbar() {
   const location = useLocation();
   const showSignButton =
     ['/signup', '/singin'].includes(location.pathname) === false;
 
+  const user = useUser();
+
+  const handleLogout = useCallback(() => {
+    signOut(auth);
+  }, []);
+
+  const renderButton = useCallback(() => {
+    if (user != null) {
+      return <Button onClick={handleLogout}>로그아웃</Button>;
+    }
+
+    if (showSignButton) {
+      return (
+        <Link to='/signin'>
+          <Button>로그인/회원가입</Button>
+        </Link>
+      );
+    }
+
+    return null;
+  }, [user, showSignButton, handleLogout]);
+
   return (
     <Flex justify='space-between' align='center' css={navbarContainerStyles}>
       <Link to='/'>홈</Link>
-      {showSignButton ? (
-        <Link to='/signup'>
-          <Button>로그인/회원가입</Button>
-        </Link>
-      ) : null}
+      {renderButton()}
     </Flex>
   );
 }
