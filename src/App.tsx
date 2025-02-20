3f3ef70c068b2ca92f3a0fdafc5988ec72d8ddc4
강동욱 <ehddnr8563@gmail.com>
2025-02-20 19:30:31 +0900

    feat: 카드 신청 페이지 - 라우팅, 접근 권한 설정

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import TestPage from '@pages/Test';
 import CardPage from '@pages/Card';
 import SignupPage from '@pages/Signup';
 import SigninPage from '@pages/Signin';
+import ApplyPage from '@pages/Apply';
 import Navbar from '@shared/Navbar';
+import PrivateRoute from '@components/auth/PrivateRoute';
 
 function App() {
   return (
@@ -18,6 +20,14 @@ function App() {
         <Route path='/signin' Component={SigninPage} />
         <Route path='/signup' Component={SignupPage} />
         <Route path='/card/:id' Component={CardPage} />
+        <Route
+          path='/apply/:id'
+          element={
+            <PrivateRoute>
+              <ApplyPage />
+            </PrivateRoute>
+          }
+        />
         <Route path='/test' Component={TestPage} />
       </Routes>
     </BrowserRouter>
