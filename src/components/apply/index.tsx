6d184ef9f258b287e80c3b1ddcdfaeed9afbe5d8
강동욱 <ehddnr8563@gmail.com>
2025-02-25 16:20:50 +0900

    feat: 카드신청 중도이탈 ux개선

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -16,31 +16,43 @@ export default function Apply({
   const user = useUser();
   const { id } = useParams();
 
-  const [step, setStep] = useState(0);
+  const storageKey = `applied-${user?.uid}-${id}`;
 
-  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>({
-    userId: user?.uid,
-    cardId: id,
+  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>(() => {
+    const applied = localStorage.getItem(storageKey);
+
+    if (applied == null) {
+      return {
+        userId: user?.uid,
+        cardId: id,
+        step: 0,
+      };
+    }
+
+    return JSON.parse(applied);
   });
 
   useEffect(() => {
-    if (step === 3) {
+    if (applyValues.step === 3) {
+      localStorage.removeItem(storageKey);
+
       onSubmit({
         ...applyValues,
         appliedAt: new Date(),
         status: APPLY_STATUS.READY,
       } as ApplyValues);
+    } else {
+      localStorage.setItem(storageKey, JSON.stringify(applyValues));
     }
-  }, [applyValues, onSubmit, step]);
+  }, [applyValues, onSubmit, storageKey]);
 
   // Terms, BasicInfo, CardInfo 스텝들의 값들을 받아오는 함수
   const handleTermsChange = (terms: ApplyValues['terms']) => {
     setApplyValues((prevValues) => ({
       ...prevValues,
       terms,
+      step: (prevValues.step as number) + 1,
     }));
-
-    setStep((prevStep) => prevStep + 1);
   };
 
   const handleBasicInfoChange = (
@@ -49,9 +61,8 @@ export default function Apply({
     setApplyValues((prevValues) => ({
       ...prevValues,
       ...infoValues,
+      step: (prevValues.step as number) + 1,
     }));
-
-    setStep((prevStep) => prevStep + 1);
   };
 
   const handleCardInfoChange = (
@@ -60,16 +71,19 @@ export default function Apply({
     setApplyValues((prevValues) => ({
       ...prevValues,
       ...cardInfoValues,
+      step: (prevValues.step as number) + 1,
     }));
-
-    setStep((prevStep) => prevStep + 1);
   };
 
   return (
     <div>
-      {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
-      {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
-      {step === 2 ? <CardInfo onNext={handleCardInfoChange} /> : null}
+      {applyValues.step === 0 ? <Terms onNext={handleTermsChange} /> : null}
+      {applyValues.step === 1 ? (
+        <BasicInfo onNext={handleBasicInfoChange} />
+      ) : null}
+      {applyValues.step === 2 ? (
+        <CardInfo onNext={handleCardInfoChange} />
+      ) : null}
     </div>
   );
 }
